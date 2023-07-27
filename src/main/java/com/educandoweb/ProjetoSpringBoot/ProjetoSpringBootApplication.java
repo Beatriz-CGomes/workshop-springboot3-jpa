831d9566e7ca8447f3fe6cc5143fbbfb00910230
Beatriz-CGomes <Beatriz.cg15@hotmail.com>
2023-07-26 23:08:16 -0300

    [200~Project created

---
+++ src/main/java/com/educandoweb/ProjetoSpringBoot/ProjetoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.ProjetoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootApplication.class, args);
+	}
+
+}
